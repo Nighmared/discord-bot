c1bb0bdbd0f532b7b4142d98c8a347c9c58f55fc
konrathj <konrathj@student.ethz.ch>
2020-12-26 20:56:14 +0100

    asdf

--- dbhandler.py
+++ dbhandler.py
@@ -16,6 +16,7 @@ class dbhandler:
 
 	def add_user(self,uid:int, name:str, permlev:int):
 		try:
+			print(f'''INSERT INTO users(uid, permlevel, name) VALUES({uid},{permlev},{name}''')
 			self.cursor.execute(f'''INSERT INTO users(uid, permlevel, name) VALUES({uid},{permlev},{name}''')
 			self.conn.commit()
 			return 0
