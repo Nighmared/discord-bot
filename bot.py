ea76b6173aae08a758e628abdc42aa0258fd5631
konrathj <konrathj@student.ethz.ch>
2020-12-27 14:02:32 +0100

    changesssss

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ async def commandHandler(message:discord.message,permlevel:int) -> int:
 
 	elif(cmd == "help" and perm_valid(cmd,permlevel)):
 			cmds = handler._execComm('''SELECT cmdname,helptext from commands where enabled==1''',True)
-			error = tryForbidden(message.channel.send,str(cmds))
+			error = await tryForbidden(message.channel.send,str(cmds))
 
 	elif(cmd =="setcache" and perm_valid(cmd,permlevel)):
 		try:
