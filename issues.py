019b3ee18a4619d425a779c18bf7d4cb90872dc7
konrathj <konrathj@student.ethz.ch>
2020-12-27 20:23:02 +0100

    changesssss

---
+++ issues.py
@@ -0,0 +1,16 @@
+import requests
+repo_name = "discord-bot"
+author = "nighmared"
+
+def getIssues()->list:
+	url = f"https://api.github.com/repos/{author}/{repo_name}/issues"
+	r = requests.get(url)
+	if(r.status_code != 200):
+		print(r.status_code,"fck")
+		return [(-1,-1)]
+	else:
+		res = r.json()
+		out = []
+		for issue in res:
+			out.append((issue["number"],issue["title"]))
+		return out.sort()
