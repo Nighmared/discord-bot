dc06ba8a704d62eeb85616c80fac6908c11193fe
konrathj <konrathj@student.ethz.ch>
2020-12-27 23:21:00 +0100

    changesssss

--- dbhandler.py
+++ dbhandler.py
@@ -86,7 +86,6 @@ class dbhandler:
 		self.conn.commit()
 	
 	def fixissue(self,id:int):
-		print("HIIII")
 		self.cursor.execute(f'''DELETE FROM issues WHERE id=={id}''')
 
 	def cmd_is_enabled(self,cmd:str)->bool:
