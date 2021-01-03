2203142cfc6117c51d0366a549ed0aab292ef467
konrathj <konrathj@student.ethz.ch>
2021-01-03 22:37:38 +0100

    changesssss

--- neko.py
+++ neko.py
@@ -7,9 +7,9 @@ answers = (
 	"its a no from me, but I heard some of the other bots aren't so dedicated to quality",
 	"fuck off mate!",
 	"ETH is far more important thank looking at such disgusting imagery",
-	"You shall pass BP, not looking to fulfil such earthly cravings",
+	"You should learn to pass BP, not be looking to fulfil such earthly cravings",
 	"You shall not pass (BP)!",
-	"Ueli doesn't like you anymore"
+	"Ueli doesn't like you anymore",
 )
 
 def getNeko():
