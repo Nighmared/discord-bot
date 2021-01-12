93d564a859276aa19be0fe58abcc2bbfcb5f116c
konrathj <konrathj@student.ethz.ch>
2021-01-12 19:01:27 +0100

    changesssss

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ async def on_message(message:discord.message):
 				modulenames+= module.__name__ +"\n"
 				try:
 					for subimport in module.IMPORTS:
-						modulenames+= f"\t|-{subimport}\n"
+						modulenames+= f"\t|-{subimport.__name__}\n"
 				except:
 					continue
 			
