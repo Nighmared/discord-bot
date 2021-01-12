159a7ca990a9725cc209d63c1faf01c444ef8d6b
konrathj <konrathj@student.ethz.ch>
2021-01-12 23:05:26 +0100

    changesssss

--- commandhandler.py
+++ commandhandler.py
@@ -89,6 +89,7 @@ class commandhandler:
 				currFieldCont = ""
 				currFieldIndex = 1
 				for (cmdn,text,alias) in final_cmd:
+					print(cmd)
 					txt = f'`{self.PREFIX}{cmdn}` (`{self.PREFIX}{alias}`)\t {text.replace("_"," ")}\n'
 					if(len(currFieldCont+txt)>2000):
 						print("adding field lol")
