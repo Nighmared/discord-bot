ff2d4d9c1a8020e1c2334d00ef9311c287967e25
konrathj <konrathj@student.ethz.ch>
2021-01-19 21:05:17 +0100

    changesssss

--- commandhandler.py
+++ commandhandler.py
@@ -358,7 +358,7 @@ class commandhandler:
 						continue
 					if(len(curr_page_cont+line)+2>self.EMBEDSIZELIMIT):
 						embObj.add_field(name=f"Page {curr_page_num}",value=curr_page_cont,inline=False)
-						curr_page_cont = ""
+						curr_page_cont = line
 						curr_page_num+=1
 					else:
 						curr_page_cont+=line+"\n"
