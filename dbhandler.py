0993c5b991d0d8e726ebf7058e58a2a12c375e16
konrathj <konrathj@student.ethz.ch>
2021-03-22 14:32:55 +0100

    changesssss

--- dbhandler.py
+++ dbhandler.py
@@ -28,16 +28,18 @@ class dbhandler:
 		return 0
 
 	def increment_user_message_count(self,author_uid:int,name:str,mention):
-		self.cursor.execute('''SELECT uid,msgcount,name FROM users;''')
+		self.cursor.execute('''SELECT uid,msgcount,name,readable_name FROM users;''')
 		uid_dict = {}
-		for uid,msgcount,uname in self.cursor.fetchall():
-			uid_dict[uid] = (int(msgcount),uname)
+		for uid,msgcount,uname,display_name in self.cursor.fetchall():
+			uid_dict[uid] = (int(msgcount),uname,display_name)
 		if(author_uid in uid_dict.keys()):
 			if(uid_dict[author_uid][1] != mention):
 				self.cursor.execute(f'''UPDATE users SET name='{mention}' where uid='{author_uid}' ''')
+			if(uid_dict[author_uid][2] != name):
+				self.cursor.execute('''UPDATE users SET readable_name=? WHERE uid=?''',(name,author_uid))
 			self.cursor.execute(f'''UPDATE users SET msgcount={uid_dict[author_uid][0]+1} WHERE uid='{author_uid}' ''')
 		else:
-			self.cursor.execute(f'''INSERT INTO users(uid,permlevel,name,msgcount) VALUES({author_uid},0,'{mention}',1)''')
+			self.cursor.execute(f'''INSERT INTO users(uid,permlevel,name,msgcount,readable_name) VALUES(?,?,?,?,?)''',(author_uid,0,mention,1,name))
 
 	def get_most_messages(self):
 		self.cursor.execute('''SELECT name,msgcount FROM users ORDER BY msgcount DESC''')
