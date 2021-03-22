37872979fd7a1453d1e184d1454d871303b9db0b
konrathj <konrathj@student.ethz.ch>
2021-03-22 15:09:12 +0100

    changesssss

--- stalk.py
+++ stalk.py
@@ -8,7 +8,7 @@ async def stalk(message:discord.Message)->tuple:
 		args = message.content[1:].split(" ")
 		target_id = args[1]
 		target_user = await message.guild.fetch_member(target_id)
-		embObj = discord.Embed(title="Stalking", description =f"Info about {target_user.mention}", color = self.TRACKERCOLOR)
+		embObj = discord.Embed(title="Stalking", description =f"Info about {target_user.mention}")
 		embObj.add_field(name="Username",value=f"{target_user.name}#{target_user.discriminator}")
 		embObj.add_field(name="UID", value=target_user.id)
 		embObj.add_field(name="Account Created", value=target_user.created_at)
@@ -16,5 +16,5 @@ async def stalk(message:discord.Message)->tuple:
 		embObj.set_image(url=target_user.avatar_url)
 		return (0, embObj)
 	except Exception as e:
-		embObj = discord.Embed(title="Stalking", description = str(e), color =self.ERRORCOLOR)
+		embObj = discord.Embed(title="Stalking", description = str(e))
 		return (1,embObj)
