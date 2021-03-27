e0443697f766bdc2f0ec8c17915c6749a90426a5
konrathj <konrathj@student.ethz.ch>
2021-03-27 23:32:34 +0100

    changesssss

---
+++ robohash.py
@@ -0,0 +1,10 @@
+import discord
+import requests
+
+BASE_URL = "https://robohash.org/"
+
+def get_embed(arg:str)->discord.Embed:
+	embObj = discord.Embed(title="Robohash", description="RoboHash image generated from your input")
+	url = BASE_URL+arg.replace(" ","%20")
+	embObj.set_image(url=url)
+	return embObj
