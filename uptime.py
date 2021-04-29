b730cd3dcc85d5243967fd6e1bd40ae407d73145
konrathj <konrathj@student.ethz.ch>
2021-04-29 14:31:43 +0200

    maybe added week counter to uptime

--- uptime.py
+++ uptime.py
@@ -21,15 +21,20 @@ class uptime:
 		mins %= 60
 		hours %= 24
 		days %= 365
+		weeks = int(days/7)
+		days %= 7
 		out = ""
 
 		if years>0 :
 			out+= f"{years} Year"+(" ","s ")[years>1]
-		if days>0 :
+		if weeks>0:
+			out+= f"{weeks} Week"+(" ","s ")[weeks>1]
+
+		if days>0 and years<1:
 			out+= f"{days} Day"+(" ","s ")[days>1]
-		if hours>0 :
+		if hours>0 and years<1:
 			out+= f"{hours} Hour"+(" ","s ")[hours>1]
-		if mins>0 :
+		if mins>0 and weeks<1:
 			out+= f"{mins} Minute"+(" ","s ")[mins>1]
 		if days<1 and secs>0 :
 			out+= f"{secs} Second"+("","s")[secs>1]
