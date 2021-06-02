d387ec32fcb08bf3f1573ce50a92768a59cbf54b
konrathj <konrathj@student.ethz.ch>
2021-06-02 16:24:51 +0200

    fixed some time formatting thing

--- src/commandhandler.py
+++ src/commandhandler.py
@@ -454,9 +454,13 @@ class commandhandler:
 			mins = int(time_diff/60)
 			time_diff %= 60
 			if hours>0:
-				return f"{hours}h {mins}m"
+				if mins>0:
+					return f"{hours}h {mins}m"
+				return f"{hours}h"
 			elif mins>0:
-				return f"{mins}m {time_diff}s"
+				if time_diff>0:
+					return f"{mins}m {time_diff}s"
+				return f"{mins}m"
 			else:
 				return f"{time_diff}s"
 
@@ -470,7 +474,7 @@ class commandhandler:
 		curr_time = int(current_time_sec())
 		emb_obj = discord.Embed(title="Loops",description="How long since each loop was last seen alive",color=self.SYSTEMCOLOR)
 		for loopname,lastseen,freq in loops:
-			emb_obj.add_field(name=loopname,value=f"{time_string(curr_time-lastseen)}(should be < {time_string(freq)}",inline=True)
+			emb_obj.add_field(name=loopname,value=f"{time_string(curr_time-lastseen)}(should be ≤ {time_string(freq)})",inline=True)
 		
 		return 0,emb_obj
 		
