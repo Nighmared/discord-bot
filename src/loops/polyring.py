996a0a0569f557c74c12ce9452d1631b7e26e152
konrathj <konrathj@student.ethz.ch>
2021-06-12 17:01:19 +0200

    updated emote id

--- src/loops/polyring.py
+++ src/loops/polyring.py
@@ -131,7 +131,7 @@ class PolyringFetcher(discord.ext.commands.Cog):
 			logging.debug("fetched polyring channel:"+str(discord_chan))
 			try:
 				msg = await discord_chan.send(embed = post.embed() )
-				await msg.add_reaction("<:yay:778745219733520426>")
+				await msg.add_reaction("<:yay:851469734545588234>")
 			except Forbidden as e:
 				if channel_id == 833645549742981190:
 					logger.fatal("Got Forbidden when trying to post a new polyring post. time to ping lukas!")
