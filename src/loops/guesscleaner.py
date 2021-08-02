766b26cb5d8ae29fd7cf3e17021c10f10e3df5e7
Jonas Konrath <jonas.konrath@icloud.com>
2021-08-02 17:29:33 +0200

    changed seconds to minutes

--- src/loops/guesscleaner.py
+++ src/loops/guesscleaner.py
@@ -18,7 +18,7 @@ class GuessCleaner(discord.ext.commands.Cog):
 
 		self.cleaner.start()
 
-	@tasks.loop(seconds=3600)
+	@tasks.loop(minutes=60)
 	async def cleaner(self):
 		logger.info("Cleaner loop got called")
 		ltime = localtime()
--- src/loops/polyring.py
+++ src/loops/polyring.py
@@ -26,7 +26,7 @@ class PolyringFetcher(discord.ext.commands.Cog):
 
 		self.getnews.start()
 
-	@tasks.loop(seconds=300)
+	@tasks.loop(minutes=5)
 	async def getnews(self):
 		if not self.dbhandler:
 			return
